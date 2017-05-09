ba0e1ce1019d0c8559776186fc95e15349c45dfd
Steven Pledger <steev@yourewinner.com>
2017-05-08 19:28:49 -0700

    Fix crash

--- app/src/main/java/com/yourewinner/yourewinner/ParticipatedPostsActivity.java
+++ app/src/main/java/com/yourewinner/yourewinner/ParticipatedPostsActivity.java
@@ -1,11 +1,14 @@
 package com.yourewinner.yourewinner;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
-public class ParticipatedPostsActivity extends AppCompatActivity {
+import java.util.Map;
+
+public class ParticipatedPostsActivity extends AppCompatActivity implements PostsAdapter.OnItemClickedListener {
 
     public final static String ARG_USERNAME = "ARG_USERNAME";
 
@@ -32,5 +35,16 @@ public class ParticipatedPostsActivity extends AppCompatActivity {
     private void createFragment() {
         Fragment fragment = PostsFragment.newInstance(PostsFragment.POS_PARTICIPATED, mUsername);
         getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, fragment).commit();
+    }
+
+    @Override
+    public void onItemClicked(Map<String, Object> item) {
+        // Load topic
+        String topicID = (String) item.get("topic_id");
+        String boardID = (String) item.get("forum_id");
+        Intent intent = new Intent(this, TopicViewActivity.class);
+        intent.putExtra(TopicViewActivity.ARG_TOPIC_ID, topicID);
+        intent.putExtra(TopicViewActivity.ARG_BOARD_ID, boardID);
+        startActivity(intent);
     }
 }
